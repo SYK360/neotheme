f0d2d527e0e3bfbf1cdde9ba52dd288264fd865e
Carlos Garcia Gomez <neorazorx@gmail.com>
2022-04-25 11:03:50 +0200

    Solucionados bugs con la carga de custom.css y custom.js

--- Assets/JS/neotheme.js
+++ Assets/JS/neotheme.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
-    $.getScript("/Dinamic/Assets/JS/Custom.js", function (data) {});
+    $.getScript("Dinamic/Assets/JS/Custom.js", function (data) {
+    });
 });
 
 function hideNeoSidebar() {
     $("#sidebar").fadeOut('fast');
     $("#sidebar-overlay").fadeOut('slow');
 }
+
 function showNeoSidebar() {
     $("#sidebar-overlay").fadeIn('slow');
     $("#sidebar").fadeIn('fast');
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Modifica el aspecto gráfico de FacturaScripts para usar un menú lateral y compacto.
 - https://facturascripts.com/plugins/neotheme
 
-### Nombre de carpeta
+## Nombre de carpeta
 Como con todos los plugins, la carpeta se debe llamar igual que el plugin. En este caso **NeoTheme**.
 
 ## Enlaces de interés
